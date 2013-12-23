94bad747d4c2a0f672f4ff3d145c54556dfe2407
geemus <geemus@gmail.com>
2013-12-23 10:32:41 -0600

    use relative paths to simplify testing

--- lib/osrcry.rb
+++ lib/osrcry.rb
@@ -1,6 +1,7 @@
-require "osrcry/contributors"
-require "osrcry/license"
-require "osrcry/version"
+dir = File.dirname(__FILE__)
+require File.join(dir, 'osrcry', 'contributors')
+require File.join(dir, 'osrcry', 'license')
+require File.join(dir, 'osrcry', 'version')
 
 module Osrcry
   # Your code goes here...
