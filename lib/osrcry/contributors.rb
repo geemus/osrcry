e8bbb703bc8316155cb4dc9e46973282d170b69d
geemus <geemus@gmail.com>
2017-04-01 10:52:14 -0500

    nicer messaging when contributors fails on new repo

--- lib/osrcry/contributors.rb
+++ lib/osrcry/contributors.rb
@@ -1,7 +1,12 @@
 module Osrcry
   class Contributors
     def self.execute
-      collaborators = `git log --pretty=format:"* %an <%ae>"`.split("\n").uniq.sort
+      collaborators = `git log --pretty=format:"* %an <%ae>" 2>&1`.split("\n").uniq.sort
+
+      if collaborators.first == "fatal: your current branch 'master' does not have any commits yet"
+        puts "Unable to create CONTRIBUTORS.md as there are no git commits.\nPlease add git commits and try again."
+        return
+      end
 
       File.open('CONTRIBUTORS.md', 'w') do |file|
         file.write(collaborators.join("\n"))
